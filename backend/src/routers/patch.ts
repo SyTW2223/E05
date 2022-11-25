49ecc7520444ce57975028fd07d1fe52a9f07069
Karina Kalwani <alu0101109046@ull.edu.es>
2022-11-25 21:16:15 +0000

    Completado patch

--- backend/src/routers/patch.ts
+++ backend/src/routers/patch.ts
@@ -1,8 +1,8 @@
 import express from 'express';
 import {Film} from '../models/film';
-// import {Book} from '../models/book';
-// import {Serie} from '../models/serie';
-// import {User} from '../models/user';
+import {Book} from '../models/book';
+import {Serie} from '../models/serie';
+import {User} from '../models/user';
 import {List} from '../models/list';
 
 
@@ -19,7 +19,7 @@ patchRouter.patch('/film', async (req, res) => {
     });
   }
 
-  const allowedUpdates = ['decription', 'rating', 'yearPublication'];
+  const allowedUpdates = ['description', 'rating', 'yearPublication'];
   const actualUpdates = Object.keys(req.body);
   const isValidUpdate = actualUpdates.every((update) => allowedUpdates.includes(update));
 
@@ -77,6 +77,114 @@ patchRouter.patch('/list', async (req: any, res) => {
     }
 
     return res.send(list);
+  } catch (error) {
+    return res.status(400).send(error);
+  }
+});
+
+/**
+ * Actualiza el libro por nombre
+ */
+ patchRouter.patch('/book', async (req: any, res) => {
+  if (!req.query.name) {
+    res.status(400).send({
+      error: 'A name must be provided',
+    });
+  }
+
+  const allowedUpdates = ['description', 'rating'];
+  const actualUpdates = Object.keys(req.body);
+  const isValidUpdate =
+    actualUpdates.every((update) => allowedUpdates.includes(update));
+
+  if (!isValidUpdate) {
+    res.status(400).send({
+      error: 'Update is not permitted',
+    });
+  }
+  try {
+    const book = await Book.findOneAndUpdate({name: req.query.name.toString()}, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!book) {
+      return res.status(404).send();
+    }
+
+    return res.send(book);
+  } catch (error) {
+    return res.status(400).send(error);
+  }
+});
+
+/**
+ * Actualiza una serie por nombre
+ */
+ patchRouter.patch('/serie', async (req: any, res) => {
+  if (!req.query.name) {
+    res.status(400).send({
+      error: 'A name must be provided',
+    });
+  }
+
+  const allowedUpdates = ['description', 'seasons', 'rating'];
+  const actualUpdates = Object.keys(req.body);
+  const isValidUpdate =
+    actualUpdates.every((update) => allowedUpdates.includes(update));
+
+  if (!isValidUpdate) {
+    res.status(400).send({
+      error: 'Update is not permitted',
+    });
+  }
+  try {
+    const serie = await Serie.findOneAndUpdate({name: req.query.name.toString()}, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!serie) {
+      return res.status(404).send();
+    }
+
+    return res.send(serie);
+  } catch (error) {
+    return res.status(400).send(error);
+  }
+});
+
+/**
+ * Actualiza el usuario por username
+ */
+ patchRouter.patch('/user', async (req: any, res) => {
+  if (!req.query.username) {
+    res.status(400).send({
+      error: 'A username must be provided',
+    });
+  }
+
+  const allowedUpdates = ['name', 'password', 'email', 'list'];
+  const actualUpdates = Object.keys(req.body);
+  const isValidUpdate =
+    actualUpdates.every((update) => allowedUpdates.includes(update));
+
+  if (!isValidUpdate) {
+    res.status(400).send({
+      error: 'Update is not permitted',
+    });
+  }
+  try {
+    const user = await User.findOneAndUpdate({username: req.query.username.toString()}, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!user) {
+      return res.status(404).send();
+    }
+
+    return res.send(user);
   } catch (error) {
     return res.status(400).send(error);
   }
