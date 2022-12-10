d0d906270c7d972cf7614cc0339084f0008e8924
Karina Kalwani <alu0101109046@ull.edu.es>
2022-12-10 13:57:32 +0000

    config inicial de tests de backend con mocha

---
+++ backend/src/app.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+import './db/mongoose';
+import {postRouter} from './routers/post';
+import {getRouter} from './routers/get';
+import {patchRouter} from './routers/patch';
+import {deleteRouter} from './routers/delete';
+import {defaultRouter} from './routers/default';
+
+const cors = require("cors");
+export const app = express();
+
+app.use(cors({
+  origin:"http://localhost:8080"
+}));
+
+app.use(express.json());
+app.use(postRouter);
+app.use(getRouter);
+app.use(patchRouter);
+app.use(deleteRouter);
+app.use(defaultRouter);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is up on port http://localhost:${port}`);
+});
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,24 +1,10 @@
-import express from 'express';
-import './db/mongoose';
-import {postRouter} from './routers/post';
-import {getRouter} from './routers/get';
-import {patchRouter} from './routers/patch';
-import {deleteRouter} from './routers/delete';
-import {defaultRouter} from './routers/default';
+import {app} from './app';
 
 const cors = require("cors");
-const app = express();
 
 app.use(cors({
   origin:"http://localhost:8080"
 }));
-
-app.use(express.json());
-app.use(postRouter);
-app.use(getRouter);
-app.use(patchRouter);
-app.use(deleteRouter);
-app.use(defaultRouter);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
