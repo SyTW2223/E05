618e7842950c457acfb13f32e8541b205204efbb
Karina Kalwani <alu0101109046@ull.edu.es>
2022-12-29 19:18:10 +0000

    initial home done

--- src/components/home/home.component.js
+++ src/components/home/home.component.js
@@ -11,25 +11,29 @@ export default function Home() {
   return (
     <MDBContainer>
       <div className='welcome'>
-        <h1 class="text-center">¡Bienvenido a MyStoryApp!</h1>
-        <p class="text-center">Con esta página web podrás estar al día con tus series, películas y libros favoritos <br/>personalizando tus propias listas de visionado.</p>
+        <h1 class="text-center my-5">¡Bienvenido a MyStoryApp!</h1>
+        <p class="text-center my-5">Con esta página web podrás estar al día con tus series, películas y libros favoritos <br/>personalizando tus propias listas de visionado.</p>
+      </div>
+
+      <div class="mt-5 mb-3">
+        <h2 class="text-center">Últimos Añadidos</h2>
       </div>
       <MDBRow>
         <MDBCarousel showControls showIndicators fade>
           <MDBCarouselItem
-            className='w-100 d-block'
+            className='w-70 d-block rounded-pill'
             itemId={1}
             src={require('../../assets/avatar.jpg')}
             alt='Cartelera Avatar 2'
           />
           <MDBCarouselItem
-            className='w-100 d-block'
+            className='w-70 d-block rounded-pill'
             itemId={2}
             src={require('../../assets/lcdp.jpg')}
             alt='Cartelera La Casa de Papel 5'
           />
           <MDBCarouselItem
-            className='w-100 d-block'
+            className='w-70 d-block rounded-pill'
             itemId={3}
             src={require('../../assets/cuando-era-divertido.jpg')}
             alt='Portada libro Cuando era divertido'
