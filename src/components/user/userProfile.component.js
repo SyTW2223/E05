12db870c0f512aacda89dcec9e383ed2160377f2
Andrea Hernandez <alu0101119137@ull.edu.es>
2022-12-30 13:38:36 +0000

    perfil usuario

--- src/components/user/userProfile.component.js
+++ src/components/user/userProfile.component.js
@@ -40,14 +40,14 @@ export const UserProfile = () => {
             <MDBCard className="mb-4">
               <MDBCardBody className="text-center">
                 <MDBCardImage
-                  src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp"
+                  src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava1.webp"
                   alt="avatar"
                   className="rounded-circle"
                   style={{ width: '150px' }}
                   fluid />
                 <p className="text-muted mb-4"></p>
                 <div className="d-flex justify-content-center mb-2">
-                  <MDBBtn outline className="ms-1">Esto es user</MDBBtn>
+                  <MDBBtn outline className="ms-1">Editar perfil</MDBBtn>
                 </div>
               </MDBCardBody>
             </MDBCard>
@@ -55,31 +55,32 @@ export const UserProfile = () => {
             <MDBCard>
             <MDBListGroup style={{ minWidth: '22rem' }} light>
               <MDBListGroupItem className='d-flex justify-content-between align-items-center'>
-              Listas preferidas
+                <strong>Biblioteca</strong>
               </MDBListGroupItem>
               <MDBListGroupItem className='d-flex justify-content-between align-items-center'>
-                Películas pendientes
+                Todas las películas
                 <MDBBadge pill light>
-                  14
+                  100
                 </MDBBadge>
               </MDBListGroupItem>
 
               <MDBListGroupItem className='d-flex justify-content-between align-items-center'>
-                Series pendientes
+                Todos los libros
                 <MDBBadge pill light>
-                  2
+                  100
                 </MDBBadge>
               </MDBListGroupItem>
 
               <MDBListGroupItem className='d-flex justify-content-between align-items-center'>
-                Libros pendientes
+                Todas las series
                 <MDBBadge pill light>
-                  1
+                  100
                 </MDBBadge>
               </MDBListGroupItem>
             </MDBListGroup>
             </MDBCard>
           </MDBCol>
+        
           <MDBCol lg="8">
             <MDBCard className="mb-4">
               <MDBCardBody>
@@ -112,6 +113,15 @@ export const UserProfile = () => {
                 <hr />
                 <MDBRow>
                   <MDBCol sm="3">
+                    <MDBCardText>Fecha de nacimiento</MDBCardText>
+                  </MDBCol>
+                  <MDBCol sm="9">
+                    <MDBCardText className="text-muted">Falta ponerla</MDBCardText>
+                  </MDBCol>
+                </MDBRow>
+                <hr />
+                <MDBRow>
+                  <MDBCol sm="3">
                     <MDBCardText>Authorities</MDBCardText>
                   </MDBCol>
                   <MDBCol sm="9">
@@ -121,69 +131,85 @@ export const UserProfile = () => {
               </MDBCardBody>
             </MDBCard>
 
+            <p className="text-center"><strong>Mis listas</strong></p>
             <MDBRow>
               <MDBCol md="6">
-                <MDBCard className="mb-4 mb-md-0">
-                  <MDBCardBody>
-                    <MDBCardText className="mb-4"><span className="text-primary font-italic me-1">assigment</span> Project Status</MDBCardText>
-                    <MDBCardText className="mb-1" style={{ fontSize: '.77rem' }}>Web Design</MDBCardText>
-                    <MDBProgress className="rounded">
-                      <MDBProgressBar width={80} valuemin={0} valuemax={100} />
-                    </MDBProgress>
+                <MDBCard>
+                <MDBListGroup style={{ minWidth: '22rem' }} light>
+                  <MDBListGroupItem className='d-flex justify-content-between align-items-center'>
+                    Películas pendientes
+                    <MDBBadge pill light>
+                      14
+                    </MDBBadge>
+                  </MDBListGroupItem>
 
-                    <MDBCardText className="mt-4 mb-1" style={{ fontSize: '.77rem' }}>Website Markup</MDBCardText>
-                    <MDBProgress className="rounded">
-                      <MDBProgressBar width={72} valuemin={0} valuemax={100} />
-                    </MDBProgress>
+                  <MDBListGroupItem className='d-flex justify-content-between align-items-center'>
+                    Series pendientes
+                    <MDBBadge pill light>
+                      2
+                    </MDBBadge>
+                  </MDBListGroupItem>
 
-                    <MDBCardText className="mt-4 mb-1" style={{ fontSize: '.77rem' }}>One Page</MDBCardText>
-                    <MDBProgress className="rounded">
-                      <MDBProgressBar width={89} valuemin={0} valuemax={100} />
-                    </MDBProgress>
-
-                    <MDBCardText className="mt-4 mb-1" style={{ fontSize: '.77rem' }}>Mobile Template</MDBCardText>
-                    <MDBProgress className="rounded">
-                      <MDBProgressBar width={55} valuemin={0} valuemax={100} />
-                    </MDBProgress>
-
-                    <MDBCardText className="mt-4 mb-1" style={{ fontSize: '.77rem' }}>Backend API</MDBCardText>
-                    <MDBProgress className="rounded">
-                      <MDBProgressBar width={66} valuemin={0} valuemax={100} />
-                    </MDBProgress>
-                  </MDBCardBody>
+                  <MDBListGroupItem className='d-flex justify-content-between align-items-center'>
+                    Libros pendientes
+                    <MDBBadge pill light>
+                      1
+                    </MDBBadge>
+                  </MDBListGroupItem>
+                  <MDBListGroupItem className='d-flex justify-content-between align-items-center'>
+                    Series vistas
+                    <MDBBadge pill light>
+                      10
+                    </MDBBadge>
+                  </MDBListGroupItem>
+                  <MDBListGroupItem className='d-flex justify-content-between align-items-center'>
+                    Libros favoritos
+                    <MDBBadge pill light>
+                      13
+                    </MDBBadge>
+                  </MDBListGroupItem>
+                  <MDBListGroupItem className='d-flex justify-content-between align-items-center'>
+                    Películas para repetir
+                    <MDBBadge pill light>
+                      28
+                    </MDBBadge>
+                  </MDBListGroupItem>
+                </MDBListGroup>
                 </MDBCard>
               </MDBCol>
 
               <MDBCol md="6">
-                <MDBCard className="mb-4 mb-md-0">
-                  <MDBCardBody>
-                    <MDBCardText className="mb-4"><span className="text-primary font-italic me-1">assigment</span> Project Status</MDBCardText>
-                    <MDBCardText className="mb-1" style={{ fontSize: '.77rem' }}>Web Design</MDBCardText>
-                    <MDBProgress className="rounded">
-                      <MDBProgressBar width={80} valuemin={0} valuemax={100} />
-                    </MDBProgress>
+                <MDBCard>
+                <MDBListGroup style={{ minWidth: '22rem' }} light>
+                  <MDBListGroupItem className='d-flex justify-content-between align-items-center'>
+                    Series que no me han gustado
+                    <MDBBadge pill light>
+                      1
+                    </MDBBadge>
+                  </MDBListGroupItem>
 
-                    <MDBCardText className="mt-4 mb-1" style={{ fontSize: '.77rem' }}>Website Markup</MDBCardText>
-                    <MDBProgress className="rounded">
-                      <MDBProgressBar width={72} valuemin={0} valuemax={100} />
-                    </MDBProgress>
+                  <MDBListGroupItem className='d-flex justify-content-between align-items-center'>
+                    Libros sin terminar
+                    <MDBBadge pill light>
+                      4
+                    </MDBBadge>
+                  </MDBListGroupItem>
 
-                    <MDBCardText className="mt-4 mb-1" style={{ fontSize: '.77rem' }}>One Page</MDBCardText>
-                    <MDBProgress className="rounded">
-                      <MDBProgressBar width={89} valuemin={0} valuemax={100} />
-                    </MDBProgress>
-
-                    <MDBCardText className="mt-4 mb-1" style={{ fontSize: '.77rem' }}>Mobile Template</MDBCardText>
-                    <MDBProgress className="rounded">
-                      <MDBProgressBar width={55} valuemin={0} valuemax={100} />
-                    </MDBProgress>
-
-                    <MDBCardText className="mt-4 mb-1" style={{ fontSize: '.77rem' }}>Backend API</MDBCardText>
-                    <MDBProgress className="rounded">
-                      <MDBProgressBar width={66} valuemin={0} valuemax={100} />
-                    </MDBProgress>
-                  </MDBCardBody>
+                  <MDBListGroupItem className='d-flex justify-content-between align-items-center'>
+                    Series sin terminar
+                    <MDBBadge pill light>
+                      3
+                    </MDBBadge>
+                  </MDBListGroupItem>
+                  <MDBListGroupItem className='d-flex justify-content-between align-items-center'>
+                    Películas sin terminar
+                    <MDBBadge pill light>
+                      5
+                    </MDBBadge>
+                  </MDBListGroupItem>
+                </MDBListGroup>
                 </MDBCard>
+                  
               </MDBCol>
             </MDBRow>
           </MDBCol>
