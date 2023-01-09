ef83ff13ee51ca24b4db0fb386dfce7d11aea48e
alu0101164403 <alu0101164403@ull.edu.es>
2023-01-09 15:36:29 +0000

    add book routes verifytoken

--- src/routes/book.routes.js
+++ src/routes/book.routes.js
@@ -1,18 +1,20 @@
+const auth = require("../middlewares/authJwt");
+
 module.exports = app => {
   const book = require("../controllers/book.controller");
   var router = require("express").Router();
 
   router
   .route("/")
-  .post(book.create)
+  .post([auth.verifyToken, auth.isAdmin], book.create)
   .get(book.findAll)
-  .delete(book.deleteAll);
+  .delete([auth.verifyToken, auth.isAdmin], book.deleteAll);
 
   router
   .route("/:title")
   .get(book.findBook)
-  .delete(book.delete)
-  .patch(book.update);
+  .delete([auth.verifyToken, auth.isAdmin], book.delete)
+  .patch([auth.verifyToken, auth.isAdmin], book.update);
 
   app.use("/book", router);
 };
