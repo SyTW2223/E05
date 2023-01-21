4b872345bc22691a2f4e89f0afa4c27ae26307f6
Karina Kalwani <alu0101109046@ull.edu.es>
2023-01-21 13:29:11 +0000

    Completed some tests

--- __test__/register.test.js
+++ __test__/register.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "@testing-library/jest-dom";
-import { render, screen } from "../testUtils";
+import { render, screen, fireEvent } from "../testUtils";
 import { BrowserRouter as Router } from "react-router-dom";
 
 import { Register } from "../src/components/auth/register.component";
@@ -27,13 +27,28 @@ describe("Register Tests", () => {
     const usernameInput = screen.getByLabelText(/username/i);
     expect(usernameInput).toBeInTheDocument();
   });
+  it("Se debe introducir el nombre de usuario", () => {
+    const usernameInput = screen.getByLabelText(/username/i);
+    fireEvent.change(usernameInput, {target: {value: 'abc'}});
+    expect(usernameInput.value).toBe('abc');
+  });
   it("Debe haber una entrada de texto para el email", () => {
     const emailInput = screen.getByLabelText(/email/i);
     expect(emailInput).toBeInTheDocument();
   });
+  it("Se debe introducir el email", () => {
+    const emailInput = screen.getByLabelText(/email/i);
+    fireEvent.change(emailInput, {target: {value: 'abc@gmail.com'}});
+    expect(emailInput.value).toBe('abc@gmail.com');
+  });
   it("Debe haber una entrada de texto para la contraseña", () => {
     const passwordInput = screen.getByLabelText(/password/i);
     expect(passwordInput).toBeInTheDocument();
+  });
+  it("Se debe introducir la contraseña", () => {
+    const passwordInput = screen.getByLabelText(/password/i);
+    fireEvent.change(passwordInput, {target: {value: 'abc'}});
+    expect(passwordInput.value).toBe('abc');
   });
   it("Debe haber un boton para registrarse", () => {
     const loginButton= screen.getByRole('button', {name: /registrarse/i});
